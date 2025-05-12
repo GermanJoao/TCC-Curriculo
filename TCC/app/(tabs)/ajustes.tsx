f3f895fa4f2927423b5d0a56a13680278682bf14
GermanJoao <retardadeds@gmail.com>
2025-05-12 18:12:32 +0000

    meu deus

---
+++ TCC/app/(tabs)/ajustes.tsx
@@ -0,0 +1,13 @@
+import { View } from 'react-native';
+import TopTabHeader from '../../components/_TopTabHeader'; // ajuste o caminho se necessário
+
+export default function ajustesScreen() {
+  return (
+    <View style={{ flex: 1, backgroundColor: '#0B0801' }}>
+      <TopTabHeader />
+      {
+      /* conteúdo da tela */
+      }
+    </View>
+  );
+}
---
+++ TCC/app/formularioCurriculo.tsx
@@ -0,0 +1,9 @@
+import { View } from 'react-native';
+
+export default function formularioCurriculo() {
+  return (
+    <View style={{ flex: 1, backgroundColor: '#0B0801' }}>
+      
+    </View>
+  );
+}
